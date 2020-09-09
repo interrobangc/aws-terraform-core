3652d29a2d73be98ab83e93ba1f449da3a85db9b
Robert Bo Davis <bo@interrobang.consulting>
2020-09-09 08:30:55 -0400

    tf 13 fixes

--- output.tf
+++ output.tf
@@ -3,74 +3,74 @@ output "bastion_public_ip" {
 }
 
 output "vpc_ids" {
-  value = "${map(
-    "dev", "${module.dev_vpc.id}",
-    "prod", "${module.prod_vpc.id}",
-    "mgmt", "${module.mgmt_vpc.id}",
-  )}"
+  value = {
+    dev = module.dev_vpc.id
+    prod = module.prod_vpc.id
+    mgmt = module.mgmt_vpc.id
+  }
 }
 
 output "data" {
-  value = "${map(
-    "dev", map(
-      "sgs", map(
-        "public_web", "${module.dev_sg.public_web}",
-        "public_ssh", "${module.dev_sg.public_ssh}",
-        "allow_egress", "${module.dev_sg.allow_egress}",
-        "bastion_ssh", "${module.bastion.sg_ssh_allow_bastion[1]}",
-      ),
-      "subnets", map(
-        "private", "${module.dev_vpc.private_subnets}",
-        "public", "${module.dev_vpc.public_subnets}",
-        "database", "${module.dev_vpc.database_subnets}",
-        "natgw_ids", "${module.dev_vpc.natgw_ids}",
-        "azs", "${var.dev_azs}",
-      ),
-      "vars", map(
-        "database_subnet_group", "${module.dev_vpc.database_subnet_group}",
-        "elasticache_subnet_group", "${module.dev_vpc.elasticache_subnet_group}",
-        "elasticache_subnet_group_name", "${module.dev_vpc.elasticache_subnet_group_name}",
-      ),
-    ),
-    "prod", map(
-      "sgs", map(
-        "public_web", "${module.prod_sg.public_web}",
-        "public_ssh", "${module.prod_sg.public_ssh}",
-        "allow_egress", "${module.prod_sg.allow_egress}",
-        "bastion_ssh", "${module.bastion.sg_ssh_allow_bastion[2]}",
-      ),
-      "subnets", map(
-        "private", "${module.prod_vpc.private_subnets}",
-        "public", "${module.prod_vpc.public_subnets}",
-        "database", "${module.prod_vpc.database_subnets}",
-        "natgw_ids", "${module.prod_vpc.natgw_ids}",
-        "azs", "${var.prod_azs}",
-      ),
-      "vars", map(
-        "database_subnet_group", "${module.prod_vpc.database_subnet_group}",
-        "elasticache_subnet_group", "${module.prod_vpc.elasticache_subnet_group}",
-        "elasticache_subnet_group_name", "${module.prod_vpc.elasticache_subnet_group_name}",
-      ),
-    ),
-    "mgmt", map(
-      "sgs", map(
-        "public_web", "${module.mgmt_sg.public_web}",
-        "public_ssh", "${module.mgmt_sg.public_ssh}",
-        "allow_egress", "${module.mgmt_sg.allow_egress}",
-        "bastion_ssh", "${module.bastion.sg_ssh_allow_bastion[0]}",
-      ),
-      "subnets", map(
-        "private", "${module.mgmt_vpc.private_subnets}",
-        "public", "${module.mgmt_vpc.public_subnets}",
-        "database", "${module.mgmt_vpc.database_subnets}",
-        "natgw_ids", "${module.mgmt_vpc.natgw_ids}",
-        "azs", "${var.mgmt_azs}",
-      ),
-      "vars", map(
-        "database_subnet_group", "${module.mgmt_vpc.database_subnet_group}",
-        "elasticache_subnet_group", "${module.mgmt_vpc.elasticache_subnet_group}",
-        "elasticache_subnet_group_name", "${module.mgmt_vpc.elasticache_subnet_group_name}",
-      ),
-    )
-  )}"
+  value = {
+    dev = {
+      sgs = {
+        public_web = module.dev_sg.public_web
+        public_ssh = module.dev_sg.public_ssh
+        allow_egress = module.dev_sg.allow_egress
+        bastion_ssh = module.bastion.sg_ssh_allow_bastion.1
+      },
+      subnets = {
+        private = module.dev_vpc.private_subnets
+        public = module.dev_vpc.public_subnets
+        database = module.dev_vpc.database_subnets
+        natgw_ids = module.dev_vpc.natgw_ids
+        azs = var.dev_azs
+      },
+      vars = {
+        database_subnet_group = module.dev_vpc.database_subnet_group
+        elasticache_subnet_group = module.dev_vpc.elasticache_subnet_group
+        elasticache_subnet_group_name = module.dev_vpc.elasticache_subnet_group_name
+      },
+    },
+    prod = {
+      sgs = {
+        public_web = module.prod_sg.public_web
+        public_ssh = module.prod_sg.public_ssh
+        allow_egress = module.prod_sg.allow_egress
+        bastion_ssh = module.bastion.sg_ssh_allow_bastion.2
+      },
+      subnets = {
+        private = module.prod_vpc.private_subnets
+        public = module.prod_vpc.public_subnets
+        database = module.prod_vpc.database_subnets
+        natgw_ids = module.prod_vpc.natgw_ids
+        azs = var.prod_azs
+      },
+      vars = {
+        database_subnet_group = module.prod_vpc.database_subnet_group
+        elasticache_subnet_group = module.prod_vpc.elasticache_subnet_group
+        elasticache_subnet_group_name = module.prod_vpc.elasticache_subnet_group_name
+      },
+    },
+    mgmt = {
+      sgs = {
+        public_web = module.mgmt_sg.public_web
+        public_ssh = module.mgmt_sg.public_ssh
+        allow_egress = module.mgmt_sg.allow_egress
+        bastion_ssh = module.bastion.sg_ssh_allow_bastion.0
+      },
+      subnets = {
+        private = module.mgmt_vpc.private_subnets
+        public = module.mgmt_vpc.public_subnets
+        database = module.mgmt_vpc.database_subnets
+        natgw_ids = module.mgmt_vpc.natgw_ids
+        azs = var.mgmt_azs
+      },
+      vars = {
+        database_subnet_group = module.mgmt_vpc.database_subnet_group
+        elasticache_subnet_group = module.mgmt_vpc.elasticache_subnet_group
+        elasticache_subnet_group_name = module.mgmt_vpc.elasticache_subnet_group_name
+      },
+    }
+  }
 }
